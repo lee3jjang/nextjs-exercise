73ff4a8c864141442cb4f4060faea37511a573df
SangJin Lee <lee3jjang@gmail.com>
2024-05-12 21:10:00 +0900

    Setup edge functions

--- src/app/country/page.tsx
+++ src/app/country/page.tsx
@@ -1,7 +1,12 @@
 import { supabase } from "../lib/initSupabase";
 
 export default async function Page() {
-  const { data } = await supabase.from("countries").select("*");
+  // const { data } = await supabase.from("countries").select("*");
+  // console.log(data);
+
+  const { data } = await supabase.functions.invoke("hello-world", {
+    body: { name: "Functions" },
+  });
   console.log(data);
 
   return <div>page</div>;
